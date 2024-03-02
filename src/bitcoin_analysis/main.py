52ac0ecfa580d11467004e02f133044da7a4f87b
paulo123571113 <123433855+paulo123571113@users.noreply.github.com>
2024-03-02 13:22:41 -0600

    Update main.py

--- src/bitcoin_analysis/main.py
+++ src/bitcoin_analysis/main.py
@@ -1,5 +1,5 @@
 
-###################### BTC Value (USD) vs. day ###################
+############# BTC Value (USD) vs. Day with Moving Average ###########
 #####################################################################
 
 #script to graph the closing price of Bitcoin pre day with moving average
